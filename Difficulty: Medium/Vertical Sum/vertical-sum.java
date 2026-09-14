214345d86bd3498cca17c30f417dc91872fdfe08
MuddasaniMaruthi <maruthi93923@gmail.com>
2026-09-14 20:39:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Vertical Sum/vertical-sum.java
@@ -0,0 +1,55 @@
+/* Structure of binary tree node
+class Node{
+public:
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    static class Pair{
+           Node node;
+           int hd;
+
+       Pair(Node node,int hd){
+           this.node=node;
+           this.hd=hd;
+       }
+       }
+    public ArrayList<Integer> verticalSum(Node root) {
+        
+       ArrayList<Integer>list=new ArrayList<>();
+       if(root==null){
+           return list;
+       }
+       Queue<Pair>queue=new LinkedList<>();
+       TreeMap<Integer,Integer>map=new TreeMap<>();
+       queue.offer(new Pair(root,0));
+       while(!queue.isEmpty()){
+           Pair p=queue.poll();
+           Node curr=p.node;
+           int hd=p.hd;
+           if(!map.containsKey(hd)){
+               map.put(hd,curr.data);
+           }
+           else{
+               map.put(hd,map.get(hd)+curr.data);
+           }
+           if(curr.left!=null){
+               queue.offer(new Pair(curr.left,hd-1));
+           }
+           if(curr.right!=null){
+               queue.offer(new Pair(curr.right,hd+1));
+           }
+           
+       }
+       for(int values:map.values()){
+           list.add(values);
+       }
+       return list;
+    }
+}
